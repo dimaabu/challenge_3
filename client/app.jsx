db8ac2064197c02fe2d6585a453f2c7285ee5bc2
dimaabu <deemaabuzahra@outlook.com>
2020-11-04 17:23:00 +0200

    g

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,31 @@
+class Square extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: null,
+      };
+    }
+  
+    render() {
+      return (
 
+        <div>
+           <form>
+               <label>Enter your name </label>
+               <input>
+               
+               </input>
+           </form>
+        </div>
+        <button
+          className="square"
+          onClick={() => this.setState({value: 'X'})}
 
+      
+          //using arrow function when the button clicked x appear
+        >
+          {this.state.value}
+        </button>
+      );
+    }
+  }
--- server.js
+++ server.js
@@ -1,2 +1,3 @@
+app.use('/static', express.static('public'))
 
 
